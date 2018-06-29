050da0e853a0f2b715abdc679601932bd790433c
Scott Roberson <srkazan23@gmail.com>
2018-06-29 17:32:40 -0500

    Added add route and view

--- routes/add.js
+++ routes/add.js
@@ -2,9 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.post('/add', function(req, res, next) {
-  res.render('add');
-  
+router.get('/add', function(req, res, next) {
+  res.render('add', { title: 'Todo List Tracker' });
 });
 
 module.exports = router;
